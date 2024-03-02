d4e99eba4ec3406df1544b9c76dd6d40bccc8fb1
nakulvijay56 <nakulvijay56@gmail.com>
2024-03-02 22:20:59 +0530

    feature branch commit revert

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          this is changed in feature branch
+          Learn React
         </a>
       </header>
     </div>
